95c2a6d72175ea0bf9601ad3c6816f49145931b9
Berta-G <berta.gutierrez@gmail.com>
2014-01-29 11:03:30 +0000

    more refactoring

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -2,21 +2,25 @@ require './lib/grid'
 
 describe Grid do
 
+	let(:easy_sudoku) {Grid.new('015003002000100906270068430490002017501040380003905000900081040860070025037204600')}
+
 	it "should initialize 81 cells" do
-		grid = Grid.new('015003002000100906270068430490002017501040380003905000900081040860070025037204600')
-		expect(grid.cells.count).to eq(81)
+		expect(easy_sudoku.cells.count).to eq(81)
 	end
 
 	it "should not be solved when initialized" do
-		grid = Grid.new('015003002000100906270068430490002017501040380003905000900081040860070025037204600')
-		expect(grid).not_to be_solved
+		expect(easy_sudoku).not_to be_solved
 	end
 
 	it "should solve a sudoku" do
-		grid = Grid.new('015003002000100906270068430490002017501040380003905000900081040860070025037204600')
-		grid.solve_sudoku
-		expect(grid.outstanding_cells).to eq(0)
-		expect(grid).to be_solved
+		easy_sudoku.solve_sudoku
+		expect(easy_sudoku.outstanding_cells).to eq(0)
+		expect(easy_sudoku).to be_solved
+	end
+
+	it "should raise an error if the sudoku is not easy" do
+		hard_sudoku = Grid.new('015003002000100906270068430490002000001040380003905000900000040860070025000004000')
+		expect{hard_sudoku.solve_sudoku}.to raise_error
 	end
 
 end
